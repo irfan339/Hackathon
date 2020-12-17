5ad6547f45ece1cd410b8b61e28fe0d2b5686b15
Azhar Ahmed <hzhru@amazon.com>
2020-12-17 13:35:00 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {Component} from 'react';
 import QuestionAnswersTemplate from './Components/QuestionAnswersTemplate';
 import GLSetTable from './Components/GLSetTable';
-// import StepZilla from 'react-stepzilla';
+import logo from './tiffany.png';
 
 class App extends Component {
     constructor(props) {
@@ -258,13 +258,18 @@ class App extends Component {
     renderAppTitle(){
       return(
         <div>
-          <div className="container">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container title-margin">
+                <nav class="navbar navbar-expand-lg navbar-dark amazonColorDark">
                     <div class="container-fluid">
                       
                       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
+                      
+                        
+                      <div class="image-style">
+                              <a href="/"><img src={logo}></img></a>
+                            </div>
                       <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                           <li class="nav-item">
@@ -278,9 +283,9 @@ class App extends Component {
                           </li>
                         </ul>
                         <form class="d-flex fright">
-                          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                          <input class="form-control me-2 btn-txt" type="search" placeholder="Search" aria-label="Search"/>
                           
-                          <button class="btn btn-outline-success" type="submit">Search</button>
+                          <button class="btn btn-outline-success btn-search" type="submit">Search</button>
                         </form>
                       </div>
                     </div>
